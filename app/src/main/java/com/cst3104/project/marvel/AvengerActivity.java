f867da5f3a1cb453b7ccd548655d5da491f9f384
ThomasL642 <ThomasLawrence642@gmail.com>
2024-11-19 18:51:19 -0500

    add random, avenger to view

--- app/src/main/java/com/cst3104/project/marvel/AvengerActivity.java
+++ app/src/main/java/com/cst3104/project/marvel/AvengerActivity.java
@@ -1,10 +1,14 @@
 package com.cst3104.project.marvel;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import com.cst3104.project.R;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AvengerActivity extends AppCompatActivity {
 
@@ -17,8 +21,23 @@ public class AvengerActivity extends AppCompatActivity {
         // Data source
         ArrayList<Marvel> avengers = Marvel.readData(this);
 
-        Object randomElement = avengers[Math.floor(Math.random() * avengers.length())];
+        ImageView imageView = findViewById(R.id.currentAvengerView);
+        Log.d("AvengersSize", "Size: " + avengers.size());
+        // Pick a random character
+        if (!avengers.isEmpty()) {
+            Marvel randomCharacter = getRandomMarvel(avengers);
 
+            // Display the random character's image in the ImageView
+            randomCharacter.flagInImageView(imageView);
+            Log.d("ImageResource", "ID: " + randomCharacter.getDrawableId());
 
+        }
+    }
+
+    // Method to get a random Marvel character from the list
+    private Marvel getRandomMarvel(ArrayList<Marvel> avengers) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(avengers.size());
+        return avengers.get(randomIndex);
     }
 }
